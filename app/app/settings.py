fd6c4e067ed37835484865bd83ed187e74af1b1a
Kyle1297 <kyle.griffin.1297@gmail.com>
2021-06-02 04:33:52 +1000

    updated image pagination

--- app/app/settings.py
+++ app/app/settings.py
@@ -187,7 +187,7 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # rest framework configurations
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 8,
+    'PAGE_SIZE': 15,
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
 }
